805017728ff2e912a08ff36947e03eb993e58324
Meseth <n.meseth@hs-osnabrueck.de>
2023-04-12 12:42:05 +0200

    Added color sensor code examples

---
+++ examples/color_sensor.py
@@ -0,0 +1,54 @@
+# See: https://lifi.datalit.de/lifi-project/sensing-light
+
+import constants
+
+import constants
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_color_v2 import BrickletColorV2
+
+# Create an IP connection to the Brick Daemon
+ipcon = IPConnection()
+ipcon.connect(constants.HOST, constants.PORT)
+
+# Get a reference to the Rotary Encoder
+color_sensor = BrickletColorV2(constants.UID_COLOR_SENSOR, ipcon)
+
+# Define a callback function for new color values
+def new_color_value(r, g, b, c):
+    print(f"R: { r }, G: { g }, B: { b }, C: { c }")
+
+# Define a callback function for new color temperature values
+def new_color_temperature_value(color_temp):
+    print(f"Color temperature: { color_temp }")
+
+# Define a callback function for new illuminance values
+def new_illuminance_value(illuminance):
+    print(f"Illuminance: { illuminance }")
+
+# Register a callback function for new color values
+color_sensor.register_callback(BrickletColorV2.CALLBACK_COLOR, new_color_value)
+
+# Configure the color callback interval to 1 second = 1000 ms and that the value need not change
+color_sensor.set_color_callback_configuration(1000, False)
+
+# Register a callback function for new color temperature values
+color_sensor.register_callback(BrickletColorV2.CALLBACK_COLOR_TEMPERATURE, new_color_temperature_value)
+
+# Configure the color temperature callback interval to 1 seond = 1000 ms and that the value need not change
+color_sensor.set_color_temperature_callback_configuration(1000, False, "x", 0, 0)
+
+# Register a callback function for new illuminance values
+color_sensor.register_callback(BrickletColorV2.CALLBACK_ILLUMINANCE, new_illuminance_value)
+
+# Configure the illuminance callback interval to 1 seond = 1000 ms and that the value need not change
+color_sensor.set_illuminance_callback_configuration(1000, False, "x", 0, 0)
+
+# Switch on the LED of the sensor
+color_sensor.set_light(True)
+
+input("Press ENTER to turn of the sensor's LED.")
+
+# Switch of the LED of the sensor
+color_sensor.set_light(False)
+
+input("Press ENTER to exit!\n")
