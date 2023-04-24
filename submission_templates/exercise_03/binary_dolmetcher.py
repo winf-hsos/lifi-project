5e241ac3251ce42205f6d85f3f93deac18003306
Meseth <n.meseth@hs-osnabrueck.de>
2023-04-24 23:23:36 +0200

    Added templates

---
+++ submission_templates/exercise_03/binary_dolmetcher.py
@@ -0,0 +1,20 @@
+# See: https://github.com/winf-hsos/lifi-exercises/raw/main/exercises/03_exercise_on_and_off.pdf
+
+def decimal_to_binary(decimal_number):
+    # TODO: Implement this function
+    pass
+
+def binary_to_decimal(binary_number_as_string):
+    # TODO: Implement this function
+    pass
+
+# Examples
+decimal = 13
+binary = decimal_to_binary(decimal)
+print(f"The decimal number { decimal } is { binary } in binary.")
+# Desired output: The decimal number 13 is 1101 in binary.
+
+binary = "1110"
+decimal = binary_to_decimal(binary)
+print(f"The binary number { binary } is { decimal } in decimal.")
+# Desired output: The binary number 1110 is 14 in binary.
---
+++ submission_templates/exercise_03/count_control.py
@@ -0,0 +1,25 @@
+# See: https://github.com/winf-hsos/lifi-exercises/raw/main/exercises/03_exercise_on_and_off.pdf
+
+#### BOILERPLATE CODE TO CONNECT TO DEVICE ####
+import yaml
+from tinkerforge.ip_connection import IPConnection
+from tinkerforge.bricklet_rotary_encoder_v2 import BrickletRotaryEncoderV2
+
+# Load the configuration file
+with open('./config.yaml', 'r') as f:
+    config = yaml.safe_load(f)
+
+UID_ROTARY_ENCODER = config.get("uid_rotary_encoder")
+HOST = config.get("host")
+PORT = config.get("port")
+
+# Create an IP connection to the Brick Daemon
+ipcon = IPConnection()
+ipcon.connect(HOST, PORT)
+
+# Get a reference to the Rotary Encoder
+rotary = BrickletRotaryEncoderV2(UID_ROTARY_ENCODER, ipcon)
+
+#### END OF BOILERPLATE CODE TO CONNECT TO DEVICE ####
+
+# Your code follows here...
